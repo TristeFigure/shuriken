202715513823ec3f4c99b9a45734544b61c76b54
TristeFigure <clement.malliet@kisskissbankbank.com>
2016-11-20 13:40:09 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject shuriken "0.1.0"
+  :description "TristeFigure's Clojure toolbox"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
