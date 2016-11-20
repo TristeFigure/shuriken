e76b0e0a10fe32aca6b58a6e13c31fa3df2af11c
TristeFigure <clement.malliet@kisskissbankbank.com>
2016-11-20 14:05:58 +0100

    Made project clojar-ready

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shuriken "0.1.0"
+(defproject org.clojars.tristefigure/shuriken "0.1.0"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
