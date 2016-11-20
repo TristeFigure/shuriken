7a5c2d0e0eb0cde5925bdae453ba81b03c6c86ca
TristeFigure <clement.malliet@kisskissbankbank.com>
2016-11-20 13:45:42 +0100

    Added project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject shuriken "0.1.0"
   :description "TristeFigure's Clojure toolbox"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
