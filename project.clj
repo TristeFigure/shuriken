79f08e832351697477ebb2eb0496fed65e31b6ce
TristeFigure <funtain@me.com>
2017-03-25 21:42:06 +0100

    Bump version to 0.12.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.12.0"
+(defproject org.clojars.tristefigure/shuriken "0.12.1"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
