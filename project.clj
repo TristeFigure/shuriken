9155c988dc0b7630ecf73418d3f417b673b9dc19
TristeFigure <funtain@me.com>
2017-11-28 16:47:52 +0100

    file-eval, macroexpand-n, better macroexpand-do, tap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.13.4"
+(defproject org.clojars.tristefigure/shuriken "0.13.5"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
