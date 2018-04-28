af749577bab2f4a8ee069483ef14d35758780509
TristeFigure <clement.malliet@kisskissbankbank.com>
2018-04-28 13:50:22 +0200

    multi: super-method

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.13.18"
+(defproject org.clojars.tristefigure/shuriken "0.13.19"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
