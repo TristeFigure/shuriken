01fdcc462922890675ff773d6c888291ae3687b9
TristeFigure <clement.malliet@kisskissbankbank.com>
2018-04-28 18:37:08 +0200

    sequential: get-nth-in & assoc-nth-in

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.13.20"
+(defproject org.clojars.tristefigure/shuriken "0.13.21"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
