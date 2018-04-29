d41d2d21adbdff19ac2cc537ed492b1870e097f7
TristeFigure <clement.malliet@kisskissbankbank.com>
2018-04-29 13:43:35 +0200

    threading: and-> & or->

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.13.22"
+(defproject org.clojars.tristefigure/shuriken "0.13.23"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
