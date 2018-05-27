630e7239c44d195a9c6701ece1b4f39a99594069
TristeFigure <clement.malliet@kisskissbankbank.com>
2018-05-27 11:45:45 +0200

    join-words + deep-doall + exported context to lexikon

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.14.1"
+(defproject org.clojars.tristefigure/shuriken "0.14.2"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
@@ -7,6 +7,7 @@
                  [potemkin "0.4.3"]
                  [com.palletops/ns-reload "0.1.0"]
                  [dance "0.1.1"]
+                 [lexikon "0.1.0"]
 
                  ;; For monkey patches
                  [robert/hooke "1.3.0"]
