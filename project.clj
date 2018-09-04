8db54997992d25a0bda6d57d4add383d2678d6aa
TristeFigure <clement.malliet@kisskissbankbank.com>
2018-09-04 19:55:25 +0200

    Add :shuriken.spec/letfn-spec and :shuriken.spec/letfn-specs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.14.9"
+(defproject org.clojars.tristefigure/shuriken "0.14.10"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
