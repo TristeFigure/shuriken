f495faa4af342aa6973f66dc91884db444c8e61a
TristeFigure <clement.malliet@kisskissbankbank.com>
2018-09-14 11:04:18 +0200

    Fix debug-print

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.14.12"
+(defproject org.clojars.tristefigure/shuriken "0.14.13"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
