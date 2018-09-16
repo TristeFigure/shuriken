6d5321260a6ec3a47e499660ebf8dcec42861f26
TristeFigure <clement.malliet@kisskissbankbank.com>
2018-09-16 08:30:45 +0200

    Fix map-keys and map-vals order on sequences

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.14.15"
+(defproject org.clojars.tristefigure/shuriken "0.14.16"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
