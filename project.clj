ca6dfabd810157ffaea52d255c4a2e35c88b3af8
TristeFigure <clement.malliet@kisskissbankbank.com>
2018-09-29 12:27:52 +0200

    exception: silence and thrown? support regex patterns

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.14.19"
+(defproject org.clojars.tristefigure/shuriken "0.14.20"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
