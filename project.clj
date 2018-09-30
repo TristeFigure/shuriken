0465ba331fe492818230443bc77179e46b108a09
TristeFigure <clement.malliet@kisskissbankbank.com>
2018-09-30 10:26:16 +0200

    Interchange the meaning of :> and <: in order

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.14.21"
+(defproject org.clojars.tristefigure/shuriken "0.14.22"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
