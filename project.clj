f017140180fa72d9a188d8feac730dbfdadb8919
TristeFigure <clement.malliet@kisskissbankbank.com>
2018-10-26 18:27:10 +0200

    pprint-meta monkey-patch

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.14.24"
+(defproject org.clojars.tristefigure/shuriken "0.14.25"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
