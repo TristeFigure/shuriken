231ac37cd7ca45503a0a8212c45931cb75098797
TristeFigure <clement.malliet@kisskissbankbank.com>
2018-10-28 06:01:25 +0100

    monkey-patches.pprint-meta: IMeta support (vars)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.14.27"
+(defproject org.clojars.tristefigure/shuriken "0.14.27-SNAPSHOT"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
