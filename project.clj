c63b2908f3a2a1b7e2e7d97630282639d1c301bc
TristeFigure <clement.malliet@kisskissbankbank.com>
2018-10-28 20:39:47 +0100

    macro: macroexpand-do accepts quoted and unquoted code

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.14.27-SNAPSHOT"
+(defproject org.clojars.tristefigure/shuriken "0.14.28"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
