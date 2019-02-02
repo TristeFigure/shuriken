158815026d022f46e49e8cebe0d0aa99d9b2aabe
TristeFigure <clement.malliet@kisskissbankbank.com>
2019-02-02 11:43:32 +0100

    destructure: edge cases

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.14.33"
+(defproject org.clojars.tristefigure/shuriken "0.14.34"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
