e71041d3a207a80e555b1aa4d4b0859bcc6c678e
TristeFigure <clement.malliet@kisskissbankbank.com>
2019-03-08 15:34:49 +0100

    macroexpand-do: avoids overwriting macroexpansion files for manual edits

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Small yet effective Clojure weapons.
 # Usage
 
 ```clojure
-[org.clojars.tristefigure/shuriken "0.14.37"]
+[org.clojars.tristefigure/shuriken "0.14.38"]
 ```
 
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tristefigure/shuriken "0.14.37"
+(defproject org.clojars.tristefigure/shuriken "0.14.38"
   :description "TristeFigure's Clojure toolbox"
   :url "https://github.com/TristeFigure/shuriken"
   :license {:name "Eclipse Public License"
